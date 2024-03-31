8d5b21189100670f0fef11bab635c08fef4f413e
Senguttuvan Mahalingam <rmsmech@gmail.com>
2024-04-01 03:55:43 +0400

    Enough for the day

--- HaleyHelpersWeb/Utils/DBAExecuter.cs
+++ HaleyHelpersWeb/Utils/DBAExecuter.cs
@@ -25,26 +25,32 @@ namespace Haley.Utils {
         }
 
         static async Task<object> ExecuteInternal(bool isread,HttpContext context, ILogger logger, string query, params (string key, object value)[] parameters) {
-            string dbakey = string.Empty;
-            if (context == null) return new NotFoundObjectResult("HttpContext is null.");
-            if (query == null) return new NotFoundObjectResult("Query is null.");
-            var jwt = await context.ParseToken(logger);
-            if (jwt == null) return new NotFoundObjectResult("JWT token is null. Exception while trying to fetch the claims from the JWT token. Ensure the JWTBearerOptions has SaveToken set to true.");
-            dbakey = jwt?.Claims?.FirstOrDefault(p => p.Type == JWTClaimType.DBA_KEY)?.Value;
-            object result = null;
-            switch (isread) {
-                case true:
-                result = (await DBA[dbakey]?.ExecuteReader(query, null, parameters))?.Select(true)?.Convert()?.ToList();
-                break;
-                case false:
-                default:
-                result = await DBA[dbakey]?.ExecuteNonQuery(query, null, parameters);
-                break;
+            try {
+                string dbakey = string.Empty;
+                if (context == null) return new NotFoundObjectResult("HttpContext is null.");
+                if (query == null) return new NotFoundObjectResult("Query is null.");
+                var jwt = await context.ParseToken(logger);
+                if (jwt == null) return new UnauthorizedObjectResult("JWT token is null. Hint: Ensure the JWTBearerOptions has SaveToken set to true.");
+                dbakey = jwt?.Claims?.FirstOrDefault(p => p.Type == JWTClaimType.DBA_KEY)?.Value;
+                object result = null;
+                switch (isread) {
+                    case true:
+                    result = (await DBA[dbakey]?.ExecuteReader(query, null, parameters))?.Select(true)?.Convert()?.ToList();
+                    break;
+                    case false:
+                    default:
+                    result = await DBA[dbakey]?.ExecuteNonQuery(query, null, parameters);
+                    break;
+                }
+                return result;
+            } catch (Exception ex) {
+                return new BadRequestObjectResult($@"Error while trying to fetch data. {ex.Message}");
+                throw;
             }
-            return result;
         }
 
         public static object GetFirst(this object input) {
+            if (typeof(IActionResult).IsAssignableFrom(input.GetType())) return input;
             if (input is List<Dictionary<string,object>> dicList && dicList.Count() > 0 && dicList.First()?.First() != null) {
                 return dicList.First().First().Value?.ToString();
             }

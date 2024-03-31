397dba2a5f17055c1112c3b5b22aaf4ab09496f5
Senguttuvan Mahalingam <rmsmech@gmail.com>
2024-04-01 02:54:06 +0400

    Almost there

--- HaleyHelpersWeb/Utils/DBAExecuter.cs
+++ HaleyHelpersWeb/Utils/DBAExecuter.cs
@@ -26,27 +26,22 @@ namespace Haley.Utils {
 
         static async Task<object> ExecuteInternal(bool isread,HttpContext context, ILogger logger, string query, params (string key, object value)[] parameters) {
             string dbakey = string.Empty;
-            try {
-                if (context == null) return new NotFoundObjectResult("HttpContext is null.");
-                if (query == null) return new NotFoundObjectResult("Query is null.");
-                var jwt = await context.ParseToken(logger);
-                if (jwt == null) return new NotFoundObjectResult("JWT token is null. Exception while trying to fetch the claims from the JWT token. Ensure the JWTBearerOptions has SaveToken set to true.");
-                dbakey = jwt?.Claims?.FirstOrDefault(p => p.Type == JWTClaimType.DBA_KEY)?.Value;
-                object result = null;
-                switch (isread) {
-                    case true:
-                    result = (await DBA[dbakey]?.ExecuteReader(query, null, parameters))?.Select(true)?.Convert()?.ToList();
-                    break;
-                    case false:
-                    default:
-                    result = await DBA[dbakey]?.ExecuteNonQuery(query, null, parameters);
-                    break;
-                }
-                return result;
-            } catch (Exception ex) {
-                logger.LogError($@"Error while trying to execute read operation for key {dbakey} with query - {query} . {Environment.NewLine} {ex.Message}");
-                return new BadRequestObjectResult(ex.Message);
+            if (context == null) return new NotFoundObjectResult("HttpContext is null.");
+            if (query == null) return new NotFoundObjectResult("Query is null.");
+            var jwt = await context.ParseToken(logger);
+            if (jwt == null) return new NotFoundObjectResult("JWT token is null. Exception while trying to fetch the claims from the JWT token. Ensure the JWTBearerOptions has SaveToken set to true.");
+            dbakey = jwt?.Claims?.FirstOrDefault(p => p.Type == JWTClaimType.DBA_KEY)?.Value;
+            object result = null;
+            switch (isread) {
+                case true:
+                result = (await DBA[dbakey]?.ExecuteReader(query, null, parameters))?.Select(true)?.Convert()?.ToList();
+                break;
+                case false:
+                default:
+                result = await DBA[dbakey]?.ExecuteNonQuery(query, null, parameters);
+                break;
             }
+            return result;
         }
 
         public static object GetFirst(this object input) {

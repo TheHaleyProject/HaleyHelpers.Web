4e7554d5d543422b40cfd0ff9bc4e5a3f16d4230
Lingam <rmsmech@gmail.com>
2024-05-29 14:25:35 +0400

    major changes

---
+++ HaleyHelpersWeb/Models/MultipartInfo.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Haley.Models {
+    public class MultipartInfo {
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public Stream Stream { get; set; }
+    }
+}

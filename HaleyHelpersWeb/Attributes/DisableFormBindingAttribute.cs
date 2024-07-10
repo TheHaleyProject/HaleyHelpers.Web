6c97ee3d52e3cf8c8b11a37e6c269096f261f6bb
Lingam <rmsmech@gmail.com>
2024-07-10 16:02:49 +0400

    file upload in progress

---
+++ HaleyHelpersWeb/Attributes/DisableFormBindingAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Haley.Models {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false )]
+    public class DisableFormBindingAttribute : Attribute, IResourceFilter {
+        public void OnResourceExecuted(ResourceExecutedContext context) {
+            
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context) {
+            //By default, when a request is made, .Net will inspect the body and if a multi-part form data is found it will fetch it into memory or into disk.
+            //This defies our whole purpose. We should not allow default processing. It also has certain limits of upto 64K temporary files creation.
+            //We go ahead and disable default factories.
+            var factories = context.ValueProviderFactories;
+            factories.RemoveType<FormValueProviderFactory>();
+            factories.RemoveType<FormFileValueProviderFactory>();
+            factories.RemoveType<JQueryFormValueProviderFactory>();
+        }
+    }
+}
---
+++ HaleyHelpersWeb/Attributes/MultipartFormInputAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Haley.Models {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class MultipartFormInputAttribute : Attribute, IResourceFilter {
+        public void OnResourceExecuted(ResourceExecutedContext context) {
+            
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context) {
+            var request = context.HttpContext.Request;
+          
+            //If we receive multipart/form-data, return without throwing any kind of error.
+            if (request != null &&
+                request.HasFormContentType
+                && request.ContentType!.StartsWith("multipart/form-data", StringComparison.OrdinalIgnoreCase)) {
+                return;
+            }
+            context.Result = new StatusCodeResult(StatusCodes.Status415UnsupportedMediaType);
+        }
+    }
+}

83ca2e5e11d9ee3ee6626f64a85871676f6d0755
LingamHPZ4 <rmsmech@gmail.com>
2025-08-17 19:26:34 +0400

    Updated

---
+++ HaleyHelpersWeb/Models/Email/EmailData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Haley.Models {
+
+    public class EmailData : IEmailData {
+        public string[] To { get; set; }
+        public string[]? CC { get; set; }
+        public string[]? BCC { get; set; }
+        public string[]? ReplyTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string? From { get; set; }
+        public bool IsHtml { get; set; } = true;
+        public EmailData() { }
+    }
+}

2f0394b80878d7458489f39c042a37916bddaf63
LingamHPZ4 <rmsmech@gmail.com>
2025-10-26 00:30:31 +0400

    File management changes

---
+++ HaleyHelpersWeb/Models/WebFeedback.cs
@@ -0,0 +1,17 @@
+﻿using Haley.Abstractions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace Haley.Models {
+    public class WebFeedback : Feedback, IWebFeedback {
+        public HttpStatusCode StatusCode { get; set; }
+        public WebFeedback SetStatusCode(HttpStatusCode code) {
+            StatusCode = code;
+            return this;
+        }
+        public WebFeedback() {  }
+        public WebFeedback(HttpStatusCode code, string message, object result) : base(message, result) { StatusCode = code; }
+        public WebFeedback(HttpStatusCode code):this(code,null,null)  { }
+        public WebFeedback(HttpStatusCode code, string message) : this(code,message,null) { }
+    }
+}
--- HaleyHelpersWeb/Utils/WebHelperUtils.cs
+++ HaleyHelpersWeb/Utils/WebHelperUtils.cs
@@ -1,7 +1,10 @@
-﻿using Haley.Abstractions;
+﻿using Azure;
+using Azure.Core;
+using Haley.Abstractions;
 using Haley.Enums;
 using Haley.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System;
@@ -12,6 +15,100 @@ using System.Web;
 
 namespace Haley.Utils {
     public static class WebHelperUtils {
+        public static async Task SetMessage(this HttpResponse target,HttpStatusCode status, string message = null) {
+            if (target == null) return;
+            await target.SetBytes(status, string.IsNullOrWhiteSpace(message) ? Array.Empty<byte>() : Encoding.UTF8.GetBytes(message));
+        }
+
+        public static async Task SetContent(this HttpResponse target, HttpStatusCode status, HttpContent content = null) {
+            if (target == null) return;
+            target.StatusCode = (int)status;
+            if (content != null) {
+                await content.CopyToAsync(target.Body);
+            }
+        }
+
+        public static async Task SetBytes(this HttpResponse target, HttpStatusCode status, byte[] message) {
+            if (target == null) return;
+            target.StatusCode = (int)status;
+            if (message != null && message.Length > 0) {
+                await target.Body.WriteAsync(message);
+            }
+        }
+
+        public static async Task SendProxyRequest(this HttpResponse target, HttpRequest source, HttpClient client, string relativeURI, HttpMethod method) {
+
+            #region 1. CREATE PROXY REQUEST 
+            //Note that, if the relativeURI starts with a slash, then it will be treated as absolute path on the HOST.. and relative to already existing base url..
+
+            // For example, if our base url is 'https://test.one.com/pod/razor/api/ and we try to add '/va/file' as the relative url, then because of the '/' infront ofthe /va/file it will be treated as absolute to the host. So, it will become 'https://test.one.com/va/file' to avoid this, remove the '/' in the relative url.
+
+            relativeURI = relativeURI.TrimStart('/');
+            var proxyRequest = new HttpRequestMessage {
+                Method = method,
+                RequestUri = new Uri(relativeURI, UriKind.Relative)
+            };
+
+            // Stream request body if present
+            //HTTP Get cannot have request Body
+            if (!HttpMethods.IsGet(source.Method) && (source.ContentLength > 0 || source.Headers.ContainsKey("Transfer-Encoding"))) {
+
+                proxyRequest.Content = new StreamContent(source.Body);
+                //SETTING THE CONTENT TYPE HERE WILL ENDUP IN ERROR. LET US SKIP SETTING HERE AND INSTEAD, SETUP IN THE HEADERS ITSELF.
+                //// Try parse content type safely (let us handle it in headers)
+            }
+
+            #endregion
+
+            #region 2. COPY SOURCE REQUEST HEADERS TO PROXY REQUEST HEADERS
+            // COPY SOURCE REQUEST HEADERS TO THE PROXY REQUEST
+            foreach (var header in source.Headers) {
+
+                //We are facing issues, when trying to stream upload. Where, Sent 0 request bytes, but Content-length promised NNNN
+                // Let HttpClient compute the content length. Let us not set it by ourselves.
+                if (header.Key.Equals("Content-Length", StringComparison.OrdinalIgnoreCase)) continue; 
+               
+                if (!proxyRequest.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray())) {
+                    proxyRequest.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                }
+            }
+
+            //Console.WriteLine($"Proxy Content-Type: {proxyRequest.Content?.Headers.ContentType}");
+            //Console.WriteLine($"Original Content-Type: {source.ContentType}");
+            #endregion
+
+
+            // Send request and stream response
+            var response = await client.SendAsync(proxyRequest, HttpCompletionOption.ResponseHeadersRead);
+
+            target.StatusCode = (int)response.StatusCode;
+
+            #region COPY RESPONSE HEADERS TO THE SOURCE RESPONSE HEADERS 
+            //We should not forward the transfer-encoding headers
+        
+            //COPY THE RESPONSE HEADERS
+            foreach (var header in response.Headers) {
+                if (string.Equals(header.Key, "Transfer-Encoding", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(header.Key, "Content-Length", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(header.Key, "Connection", StringComparison.OrdinalIgnoreCase))
+                    continue;
+                target.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            //COPY THE RESPONSE CONTENT HEADERS
+            foreach (var header in response.Content.Headers) {
+                if (string.Equals(header.Key, "Transfer-Encoding", StringComparison.OrdinalIgnoreCase) ||
+                    string.Equals(header.Key, "Content-Length", StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                target.Headers[header.Key] = header.Value.ToArray();
+            }
+            #endregion
+            
+            // Copy the body manually
+            await response.Content.CopyToAsync(target.Body);
+        }
+
         public static object ConvertDBAResult(this object input, ResultFilter filter = ResultFilter.FirstDictionaryValue) {
             //If we send error
             if (input is FeedbackError dbaerr) {

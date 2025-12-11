ddb08d41aead433de8914d17da842093ecacc54e
LingamHPZ4 <rmsmech@gmail.com>
2025-12-11 22:19:23 +0400

    Changes to policies

--- HaleyHelpersWeb/Extensions/PolicyMakerExtensions.cs
+++ HaleyHelpersWeb/Extensions/PolicyMakerExtensions.cs
@@ -6,7 +6,7 @@ namespace Haley.Utils {
         public static void CreateAuthPolicy(this AuthorizationOptions options, string policy_name, string scheme_name, Action<AuthorizationPolicyBuilder>? processor = null) { 
             options.CreateAuthPolicy(policy_name, new string[] { scheme_name }, processor: processor);
         }
-        public static void CreateAuthPolicy(this AuthorizationOptions options, string policy_name, string[] scheme_names, string[] requiredRoles =null, string[] requiredClaims = null, Action<AuthorizationPolicyBuilder>? processor = null) {
+        public static void CreateAuthPolicy(this AuthorizationOptions options, string policy_name, string[] scheme_names, string[] requiredRoles =null, string[] requiredClaims = null, Action<AuthorizationPolicyBuilder>? processor = null, bool allRoles = false, bool allClaims = false) {
 
             if (string.IsNullOrWhiteSpace(policy_name)) throw new ArgumentNullException("Policy name cannot be null or empty");
             if (scheme_names == null || scheme_names.Count() < 1) throw new ArgumentNullException("Scheme name cannot be null or empty");
@@ -16,14 +16,30 @@ namespace Haley.Utils {
 
             var policyBuilder = new AuthorizationPolicyBuilder(schemes) //Api Key
                             .RequireAuthenticatedUser();
+
             if (requiredRoles != null && requiredRoles.Length > 0) {
-                policyBuilder
-                    .RequireRole(requiredRoles.Where(p => !string.IsNullOrWhiteSpace(p)).Distinct());
+                var roles = requiredRoles.Where(p => !string.IsNullOrWhiteSpace(p)).Distinct().ToList();
+
+                if (!allRoles) {
+                    policyBuilder.RequireRole(roles.ToArray()); //Here, role is OR condition. Presence of any role will be accepted.
+                } else {
+                    policyBuilder.RequireAssertion(ctx =>
+                     roles.All(r => ctx.User.IsInRole(r))
+                    );
+                }
             }
 
             if (requiredClaims != null && requiredClaims.Length > 0) {
-                foreach (var claim in requiredClaims.Where(p => !string.IsNullOrWhiteSpace(p)).Distinct()) {
-                    policyBuilder.RequireClaim(claim);
+                var claims = requiredClaims.Where(p => !string.IsNullOrWhiteSpace(p)).Distinct().ToList();
+                if (!allClaims) {
+                    foreach (var claim in claims) {
+                        policyBuilder.RequireClaim(claim);
+                    }
+                } else {
+                    policyBuilder.RequireAssertion(ctx =>
+                     //claims.All(c => ctx.User.HasClaim(c, ctx.User.FindFirst(c)?.Value))
+                     claims.All(rc => ctx.User.Claims.Any(c => c.Type == rc))  
+                    );
                 }
             }
 
@@ -33,6 +49,10 @@ namespace Haley.Utils {
         public static AuthPolicyMaker WithSchemes(this AuthorizationOptions options, params string[] scheme_names) => new AuthPolicyMaker() { SchemeNames = scheme_names, Options = options };
         public static AuthPolicyMaker RequireRoles(this AuthorizationOptions options, params string[] role_names) => new AuthPolicyMaker() { RoleNames = role_names, Options = options };
         public static AuthPolicyMaker RequireClaims(this AuthorizationOptions options, params string[] claim_names) => new AuthPolicyMaker() { ClaimNames = claim_names, Options = options };
+
+        public static AuthPolicyMaker RequireAllRoles(this AuthorizationOptions options, params string[] role_names) => new AuthPolicyMaker() { RoleNames = role_names, Options = options , AllRoles = true};
+        public static AuthPolicyMaker RequireAllClaims(this AuthorizationOptions options, params string[] claim_names) => new AuthPolicyMaker() { ClaimNames = claim_names, Options = options, AllClaims = true };
+
         public static AuthPolicyMaker ForPolicy(this AuthorizationOptions options, string policy_name) => new AuthPolicyMaker() { PolicyName = policy_name, Options = options };
 
         public static AuthPolicyMaker WithSchemes(this AuthPolicyMaker input, params string[] scheme_names) {
@@ -45,6 +65,17 @@ namespace Haley.Utils {
         }
         public static AuthPolicyMaker RequireClaims(this AuthPolicyMaker input, params string[] claim_names) {
             input.ClaimNames = claim_names;
+            return input;
+        }
+
+        public static AuthPolicyMaker RequireAllRoles(this AuthPolicyMaker input, params string[] role_names) {
+            input.RoleNames = role_names;
+            input.AllRoles = true;
+            return input;
+        }
+        public static AuthPolicyMaker RequireAllClaims(this AuthPolicyMaker input, params string[] claim_names) {
+            input.ClaimNames = claim_names;
+            input.AllClaims = true;
             return input;
         }
         public static AuthPolicyMaker ForPolicy(this AuthPolicyMaker input, string policy_name) {
@@ -69,7 +100,9 @@ namespace Haley.Utils {
                 scheme_names: input.SchemeNames,
                 requiredRoles: input.RoleNames, 
                 requiredClaims: input.ClaimNames, 
-                processor: processor);
+                processor: processor,
+                allRoles:input.AllRoles,
+                allClaims:input.AllClaims);
         }
     }
 }
--- HaleyHelpersWeb/Models/AuthPolicyMaker.cs
+++ HaleyHelpersWeb/Models/AuthPolicyMaker.cs
@@ -4,7 +4,9 @@ namespace Haley.Models {
     public class AuthPolicyMaker {
         public string[] SchemeNames { get; set; }
         public string[] RoleNames { get; set; }
+        public bool AllRoles { get; set; }
         public string[] ClaimNames { get; set; }
+        public bool AllClaims { get; set; }
         public string PolicyName { get; set; }
         public AuthorizationOptions Options { get; set; }
     }
